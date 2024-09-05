26af488939d3e6226a05d95ab05dab86af3b9aa6
vincyLim <lim.vincy188@1utar.my>
2024-09-05 09:49:07 +0800

    Updated firestore into registration authentication

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,24 +1,34 @@
 import React, {useState, useEffect, useContext} from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-native';
 import { AuthContext } from '../navigation/AuthProvider';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ProfileScreen = ({navigation, route}:any) => {
   const {user, logout} =  useContext(AuthContext);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome {user.email}</Text>
-      <Image source={{ uri: user.photoURL }} style={styles.profileImage} />
-      <Button title="Logout" onPress= {()=> logout()}/>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: "#fff"}}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{justifyContent: 'center', alignItems:'center'}}
+        showsVerticalScrollIndicator={false}
+      >
+        <Image style={styles.profileImage} source={{ uri: user.photoURL }}/>
+        <Text style={{}}>{user.displayName}</Text>
+
+        <View>
+          <TouchableOpacity onPress={()=>{}}>
+            <Text>Edit Profile</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     fontSize: 18,
