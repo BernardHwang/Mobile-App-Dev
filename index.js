776e9b5185f6e6845557f0af27f8acf89ceaa9ae
shirley24k <kuik0224@gmail.com>
2024-09-14 22:38:50 +0800

    update

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App3);
+AppRegistry.registerComponent(appName, () => App);
