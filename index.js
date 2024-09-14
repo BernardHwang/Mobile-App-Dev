3380d35c0a6f49dc50bef2928aa05e440e33d3ca
Bernard Hwang <147620186+BernardHwang@users.noreply.github.com>
2024-09-14 22:35:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-
-import App3 from './App3.tsx';
 import App from './App';
-
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
